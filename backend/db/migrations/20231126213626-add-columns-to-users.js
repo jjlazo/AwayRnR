41c73462c36548a8b33bc6ccb81e1abe31c9495c
juniper <jjlazo96@gmail.com>
2023-11-26 17:49:43 -0500

    add-columns-edit

--- backend/db/migrations/20231126213626-add-columns-to-users.js
+++ backend/db/migrations/20231126213626-add-columns-to-users.js
@@ -1,19 +1,26 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+  options.tableName = 'Users';
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+    await queryInterface.addColumn(options, 'firstName', {
       type: Sequelize.STRING(30)
     });
 
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn(options, 'lastName', {
       type: Sequelize.STRING(30)
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    await queryInterface.removeColumn(options, 'firstName');
+    await queryInterface.removeColumn(options, 'lastName');
   }
 };
