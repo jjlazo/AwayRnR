94d5ed95a916e0bac9e61fea592bb2045ad7805f
juniper <jjlazo96@gmail.com>
2023-11-29 16:45:51 -0500

    session route changes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -11,9 +11,21 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -79,54 +91,4 @@ router.get(
     }
 );
 
-
-const validateLogin = [
-    check('credential')
-        .exists({ checkFalsy: true })
-        .notEmpty()
-        .withMessage('Please provide a valid email or username.'),
-    check('password')
-        .exists({ checkFalsy: true })
-        .withMessage('Please provide a password.'),
-    handleValidationErrors
-];
-
-
-// Log in
-router.post(
-    '/',
-    validateLogin,
-    async (req, res, next) => {
-        const { credential, password } = req.body;
-
-        const user = await User.unscoped().findOne({
-            where: {
-                [Op.or]: {
-                    username: credential,
-                    email: credential
-                }
-            }
-        });
-
-        if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            err.title = 'Login failed';
-            err.errors = { credential: 'The provided credentials were invalid.' };
-            return next(err);
-        }
-
-        const safeUser = {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-        };
-
-        await setTokenCookie(res, safeUser);
-
-        return res.json({
-            user: safeUser
-        });
-    }
-);
 module.exports = router;
