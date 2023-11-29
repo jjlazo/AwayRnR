0887f0c33b19245138df3d828b09e976270e78b0
juniper <jjlazo96@gmail.com>
2023-11-28 20:00:59 -0500

    minor changes to users

--- backend/db/migrations/20231128003614-create-spot.js
+++ backend/db/migrations/20231128003614-create-spot.js
@@ -24,7 +24,8 @@ module.exports = {
       },
       address: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true
       },
       city: {
         type: Sequelize.STRING,
