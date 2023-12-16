ff6347a821d3eb91e283017ae5a56d0524ab4b27
juniper <jjlazo96@gmail.com>
2023-12-15 19:00:35 -0500

    phase 0

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -30,40 +30,4 @@ router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
-// for testing: [
-// router.get(
-//     '/restore-user',
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
-
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//     '/require-auth',
-//     requireAuth,
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
-// // GET /api/set-token-cookie
-// const { setTokenCookie } = require('../../utils/auth.js');
-
-// const { User } = require('../../db/models');
-
-// router.get('/set-token-cookie', async (_req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'WillyShakes3'
-//         }
-//     });
-//     setTokenCookie(res, user);
-//     return res.json({ user: user });
-// });
-
-// router.post('/test', function (req, res) {
-//     res.json({ requestBody: req.body });
-// });
-// ]
 module.exports = router;
