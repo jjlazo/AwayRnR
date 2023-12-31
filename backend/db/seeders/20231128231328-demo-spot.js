21569175d527ff6e32f70350ce58d020b9aad0cb
juniper <jjlazo96@gmail.com>
2023-12-31 14:13:44 -0500

    update seeds

--- backend/db/seeders/20231128231328-demo-spot.js
+++ backend/db/seeders/20231128231328-demo-spot.js
@@ -9,48 +9,108 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await Spot.bulkCreate([
       {
-        ownerId: 1,
-        address: '123 Disney Lane',
+        ownerId: 3,
+        address: '123 Animal Lane',
         city: 'San Francisco',
         state: 'California',
         country: 'United States of America',
         lat: 37.7645358,
         lng: -122.4730327,
-        name: "App Academy",
-        description: "Place where web developers are created",
-        price: 123
+        name: "Chez Cheddar",
+        description: "The Cheeziest Place Around!",
+        price: 79
       },
       {
-        ownerId: 2,
-        address: '456 Minnie Drive',
+        ownerId: 1,
+        address: '456 Doggie Drive',
         city: 'San Diego',
         state: 'California',
         country: 'United States of America',
         lat: 32.3646578,
         lng: -111.4730327,
-        name: "House of Mouse",
-        description: "Where Mickey is king",
+        name: "Hound House Kick",
+        description: "Dog friendly, music lover's dream.",
         price: 456
       },
       {
-        ownerId: 3,
-        address: '789 Barbie Rd',
+        ownerId: 4,
+        address: '789 Arachnid Rd',
         city: 'Dallas',
         state: 'Texas',
         country: 'United States of America',
         lat: 23.7645358,
         lng: -117.6730327,
-        name: "Mojo Dojo Casa House",
-        description: "it's kenough",
+        name: "SPIDER ISLAND",
+        description: "net advised",
+        price: 123
+      },
+      {
+        ownerId: 5,
+        address: '624 Starry Ln',
+        city: 'Dallas',
+        state: 'Texas',
+        country: 'United States of America',
+        lat: 23.7345650,
+        lng: -117.6230427,
+        name: "Stargazer's Roost",
+        description: "Telescope available!",
+        price: 123
+      },
+      {
+        ownerId: 6,
+        address: '907 Snail Slope St',
+        city: 'Ocala',
+        state: 'Florida',
+        country: 'United States of America',
+        lat: 23.7543358,
+        lng: -117.6098327,
+        name: "SCORPION ISLAND",
+        description: "BRING MEDICINE",
+        price: 123
+      },
+      {
+        ownerId: 7,
+        address: '321 Rowdy Ruff Rd',
+        city: 'Dallas',
+        state: 'Texas',
+        country: 'United States of America',
+        lat: 13.7645358,
+        lng: -117.6730327,
+        name: "HARV'S ISLAND",
+        description: "Dog friendly, guitar available",
+        price: 123
+      },
+      {
+        ownerId: 2,
+        address: '317 Bell Bag Drive',
+        city: 'New York City',
+        state: 'New York',
+        country: 'United States of America',
+        lat: 63.7645358,
+        lng: -217.6730327,
+        name: "Tom's Nook",
+        description: "Quaint and quiet, a perfect island getaway!",
+        price: 123
+      },
+      {
+        ownerId: 8,
+        address: '098 Feline Ln',
+        city: 'Denver',
+        state: 'Colorado',
+        country: 'United States of America',
+        lat: 13.7645358,
+        lng: -117.6730327,
+        name: "The Cat's Meow",
+        description: "Bring offerings",
         price: 123
       }
     ], { validate: true });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
--- backend/db/seeders/20231128233013-demo-spot-image.js
+++ backend/db/seeders/20231128233013-demo-spot-image.js
@@ -14,17 +14,42 @@ module.exports = {
     await SpotImage.bulkCreate([
       {
         spotId: 1,
-        url: 'image url',
+        url: 'https://dodo.ac/np/images/e/e8/House_of_Chadder_NL_Exterior.png',
         preview: true
       },
       {
         spotId: 2,
-        url: 'image url',
-        preview: false
+        url: 'https://dodo.ac/np/images/f/ff/RV_of_K.K._Slider_NLWa_Exterior.png',
+        preview: true
       },
       {
         spotId: 3,
-        url: 'image url',
+        url: 'https://cdn.mos.cms.futurecdn.net/BMwE72E288f9QgeRrmjvhg.jpg',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://dodo.ac/np/images/d/de/RV_of_Celeste_NLWa_Exterior.png',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://img.gamewith.net/article/thumbnail/rectangle/19265.png',
+        preview: true
+      },
+      {
+        spotId: 6,
+        url: 'https://dodo.ac/np/images/a/af/NH_Harv%27s_Island.jpg',
+        preview: true
+      },
+      {
+        spotId: 7,
+        url: 'https://dodo.ac/np/images/b/bf/RV_of_Tom_Nook_NLWa_Exterior.png',
+        preview: true
+      },
+      {
+        spotId: 8,
+        url: 'https://dodo.ac/np/images/f/fc/PG_Exterior_Islander.png',
         preview: true
       }
     ], { validate: true });
